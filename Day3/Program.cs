eba4c0587084190422c46cdb9e4f23547b155c76
Mark Gehan <markg@unity3d.com>
2017-12-07 11:46:31 -0800

    Day3 initial checkin

---
+++ Day3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 0)
+            {
+                InvalidInputMessage();
+                return;
+            }
+
+            foreach (var arg in args)
+            {
+                int inputNum;
+
+                var numMove = 0;
+                
+                if(!int.TryParse(arg, out inputNum))
+                {
+                    InvalidInputMessage();
+                    return;
+                }
+
+                int ringNumber = 0;
+                int rootNumber = 1;
+
+                while((rootNumber * rootNumber) < inputNum)
+                {
+                    ringNumber++;
+                    rootNumber += 2;
+                }
+
+                Console.WriteLine($"Ring number: {ringNumber}");
+            }
+        }
+
+        private static void InvalidInputMessage()
+        {
+            Console.WriteLine("Invalid input.");
+            Console.ReadKey();
+        }
+    }
+}
