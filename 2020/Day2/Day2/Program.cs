a946c554a66025607708cbad65faf14fc0eddab7
Mark Gehan <mark.gehan@gmail.com>
2020-12-02 07:17:43 -0500

    solved Day 2 part 1, working on Day 2 part 2

---
+++ 2020/Day2/Day2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        struct Entry
+        {
+            public int min;
+            public int max;
+            public char letter;
+            public char[] password;
+        }
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt");
+
+            var entryList = new List<Entry>();
+
+            foreach (var inputString in input)
+            {
+                var dashIndex = inputString.IndexOf('-');
+                var spaceIndex = inputString.IndexOf(' ');
+                var colonIndex = inputString.IndexOf(':');
+
+                var min = int.Parse(inputString.Substring(0, dashIndex));
+                var max = int.Parse(inputString.Substring(dashIndex+1, spaceIndex - (dashIndex+1)));
+                var letter = inputString.Substring(colonIndex -1, 1)[0];
+                var password = inputString.Substring(colonIndex+2).ToCharArray();
+                entryList.Add(new Entry {min = min, max = max, letter = letter, password = password});
+            }
+
+            int validCount = 0;
+/*
+            foreach (var entry in entryList)
+            {
+                var count = 0;
+                foreach (var letter in entry.password)
+                {
+                    if (letter == entry.letter)
+                    {
+                        count++;
+                    }
+
+                    if (count > entry.max)
+                    {
+                        break;
+                    }
+                }
+                
+                if (count <= entry.max && count >= entry.min)
+                {
+                    validCount++;
+                }
+            }
+            */
+            
+            foreach (var entry in entryList)
+            {
+
+                if (entry.min < entry.password.Length && entry.max < entry.password.Length)
+                {
+                    if (entry.password[entry.min] == entry.letter ^ entry.password[entry.max] == entry.letter)
+                    {
+                        validCount++;
+                    }
+                }
+            }
+            
+            Console.WriteLine(validCount);
+            Console.ReadKey();
+
+        }
+    }
+}
